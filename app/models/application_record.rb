d42686b335ca29841203e444a6ccbd22feb40172
efoosag <efoosagsite@gmail.com>
2023-03-01 10:44:18 +0100

    fix linters error

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 # base
 class ApplicationRecord < ActiveRecord::Base
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
